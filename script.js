ffc43bfd94e3ad830504f04f8b66647f4c9efbc1
FezodgeIII <j.pullan2000@gmail.com>
2015-05-16 20:49:53 +0100

    Deletion of Javascript Files
    I got rid of all the Javascript files because:
    1) I do not need them/don't use them
    2) The GitHub web server that I'm hosting this on doesn't support it.

--- script.js
+++
@@ -1,7 +0,0 @@
-var projectsBar = document.getElementById('projects-bar').innerHTML;
-var projectsTab = document.getElementById('projects');
-
-if (projectsTab.mouseover)
-{
-	projectsBar.setAttribute('id', 'projects-bar-show');   
-}
